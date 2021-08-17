431d0c0be8b17e012b843037007eebe69c129e19
Ricardo Alexandre Pinheiro Picolli <rickpicolli@Mac.local>
2021-08-17 08:36:20 -0700

    adding method and adding some improvments

--- js/scripts.js
+++ js/scripts.js
@@ -92,3 +92,50 @@ function showSlides() {
   }, Math.round(_this._autoSlide/2)*1000);
 };
 
+var min = '2021-08-12T00:00';
+var max = '2022-02-12T00:00';
+
+mobiscroll.datepicker('#demo-booking-multiple', {
+    controls: ['calendar'],
+    min: min,
+    max: max,
+    selectMultiple: true,
+    onInit: function (event, inst) {
+        inst.setVal([
+            '2021-08-11T00:00',
+            '2021-08-16T00:00',
+            '2021-08-17T00:00'
+        ], true);
+    },
+    onPageLoading: function (event, inst) {
+        getBookings(event.firstDay, function callback(bookings) {
+            inst.setOptions({
+                labels: bookings.labels,
+                invalid: bookings.invalid
+            });
+        });
+    }
+});
+                                  
+function getBookings(d, callback) {
+    var invalid = [],
+        labels = [];
+
+    mobiscroll.util.http.getJson('//trial.mobiscroll.com/getbookings/?year=' + d.getFullYear() + '&month=' + d.getMonth(), function (bookings) {
+        for (var i = 0; i < bookings.length; ++i) {
+            var booking = bookings[i],
+                d = new Date(booking.d);
+
+            if (booking.nr > 0) {
+                labels.push({
+                    start: d,
+                    title: booking.nr + ' SPOTS',
+                    textColor: '#e1528f'
+                });
+            } else {
+                invalid.push(d);
+            }
+        }
+        callback({ labels: labels, invalid: invalid });
+    }, 'jsonp');
+}
